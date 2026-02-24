2f721e195c0006d0d7c317b4d1e631e16d1abb99
Peter Louis Anderson <142972091+peter-louisx@users.noreply.github.com>
2026-02-24 23:13:53 +0700

    mbg scraping progress 1

--- README.md
+++ README.md
@@ -4,27 +4,27 @@
 
 ### Requirements
 
-* Node
-* NPM
+- Node
+- NPM
 
 1. Installation
 
 ```tsx
 
-npm install 
+npm install
 ```
 
-1. Run scraper&#x20;
+2. Run scraper&#x20;
 
 ```tsx
 
-npx tweet-harvest
+npm run scrape
 ```
 
 You will be prompted for these:
 
-* **X auth token** (Inspect -> Application -> Cookies -> [https://x.com](https://x.com) -> Copy `auth_token`)
-* **Keyword** (Format: \<Keyword> since:\<Start Date> until:\<End Date> lang:\<Language>  , eg: ChatGPT since:2024-10-30 until:2024-11-01 lang\:id)
-* **Format** (CSV/Excel)
+- **X auth token** (Inspect -> Application -> Cookies -> [https://x.com](https://x.com) -> Copy `auth_token`)
+- **Keyword** (Format: \<Keyword> since:\<Start Date> until:\<End Date> lang:\<Language> , eg: ChatGPT since:2024-10-30 until:2024-11-01 lang\:id)
+- **Format** (CSV/Excel)
 
 Result will be saved in `tweets-data/`
---
+++ index.js
@@ -0,0 +1,46 @@
+const limit = 1000;
+const token = "a8fb8f0108dbb116f614ab3fb8e96dcbac7ca694";
+
+const queries = {
+  MBG: [
+    // "MBG since:2025-01-01 until:2025-01-07 lang:id", 169
+    // "MBG since:2025-01-08 until:2025-01-14 lang:id", 169
+    // "MBG since:2025-01-15 until:2025-01-21 lang:id", 159
+    // "MBG since:2025-01-22 until:2025-01-28 lang:id", 129
+    // "MBG since:2025-01-29 until:2025-02-04 lang:id", 172
+    // "MBG since:2025-02-05 until:2025-02-11 lang:id", 126
+    // "MBG since:2025-02-12 until:2025-02-18 lang:id", 126
+    // "MBG since:2025-02-19 until:2025-03-04 lang:id", 147
+    // "MBG since:2025-03-05 until:2025-03-11 lang:id", 176
+    // "MBG since:2025-03-12 until:2025-03-18 lang:id", 152
+    // "MBG since:2025-03-19 until:2025-04-04 lang:id", 126
+    // "MBG since:2025-04-05 until:2025-04-11 lang:id", 114
+    // "MBG since:2025-04-12 until:2025-04-18 lang:id", 150
+    // "MBG since:2025-04-19 until:2025-05-04 lang:id", 232
+    // "MBG since:2025-05-05 until:2025-05-11 lang:id", 195
+    // "MBG since:2025-05-12 until:2025-05-18 lang:id", 200
+    // "MBG since:2025-05-19 until:2025-06-04 lang:id", 160
+    // "MBG since:2025-06-05 until:2025-06-11 lang:id", 162
+    // "MBG since:2025-06-12 until:2025-06-18 lang:id", 166
+    // "MBG since:2025-06-19 until:2025-07-04 lang:id", 208
+    // "MBG since:2025-07-05 until:2025-07-11 lang:id", 145
+    "MBG since:2025-07-12 until:2025-07-18 lang:id",
+    "MBG since:2025-07-19 until:2025-08-04 lang:id",
+    "MBG since:2025-08-05 until:2025-08-11 lang:id",
+    "MBG since:2025-08-12 until:2025-08-18 lang:id",
+    "MBG since:2025-08-19 until:2025-09-04 lang:id",
+    "MBG since:2025-09-05 until:2025-09-11 lang:id",
+    "MBG since:2025-09-12 until:2025-09-18 lang:id",
+    "MBG since:2025-09-19 until:2025-10-04 lang:id",
+    "MBG since:2025-10-05 until:2025-10-11 lang:id",
+    "MBG since:2025-10-12 until:2025-10-18 lang:id",
+    "MBG since:2025-10-19 until:2025-11-04 lang:id",
+    "MBG since:2025-11-05 until:2025-11-11 lang:id",
+    "MBG since:2025-11-12 until:2025-11-18 lang:id",
+    "MBG since:2025-11-19 until:2025-12-04 lang:id",
+    "MBG since:2025-12-05 until:2025-12-11 lang:id",
+    "MBG since:2025-12-12 until:2025-12-18 lang:id",
+    "MBG since:2025-12-19 until:2025-12-26 lang:id",
+    "MBG since:2025-12-27 until:2025-01-03 lang:id",
+  ],
+};
